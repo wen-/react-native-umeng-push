45deb050c4c30fbb75ada6050b4ef1dc2f899bbf
郑仲文 <zhengzw@zoteri.net>
2018-04-03 10:09:22 +0800

    支持安卓前台情况下能接收推送消息

--- android/src/main/java/com/liuchungui/react_native_umeng_push/UmengPushApplication.java
+++ android/src/main/java/com/liuchungui/react_native_umeng_push/UmengPushApplication.java
@@ -47,7 +47,6 @@ public class UmengPushApplication extends Application {
     //开启推送
     private void enablePush() {
         mPushAgent = PushAgent.getInstance(this);
-        mPushAgent.setNotificaitonOnForeground(false);
         //注册推送服务，每次调用register方法都会回调该接口
         mPushAgent.register(new IUmengRegisterCallback() {
             @Override
